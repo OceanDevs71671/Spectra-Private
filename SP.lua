4e8e88bb2a648258b0dc9fbe26cd2cac05c7571d
OceanDevs71671 <matthew71671@gmail.com>
2025-10-17 19:23:48 +0100

    Update SP.lua

--- SP.lua
+++ SP.lua
@@ -1,1 +1,12 @@
 print("Welcome to Spectra Private, if this script has any bugs, report to the Github!")
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/cat"))() --you can go into the github link and copy all of it and modify it for yourself.
+local Window = Library:CreateWindow("cattoware UI Doc", Vector2.new(492, 598), Enum.KeyCode.RightControl) --you can change your UI keybind
+local AimingTab = Window:CreateTab("Tab 1") --you can rename this tab to whatever you want --you can also change the tabs code, for example "AimingTab" can be changed to "FunnyCoolTab" etc.
+
+
+local testSection = AimingTab:CreateSector("First Section", "left")  --you can  change the section code, for example "testsection" can be changed to "FunnyCoolSection" etc.
+
+testSection:AddToggle("Toggle", false, function(first)
+    print("ejejejejejeje")
+end)
